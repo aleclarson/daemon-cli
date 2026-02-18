cfff812a7f731e20c772b0ac4a272f329a48722d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-02-18 15:19:48 -0500

    fix: completion bugs

--- src/lib/completion.ts
+++ src/lib/completion.ts
@@ -13,7 +13,7 @@ export function getZshCompletionScript(): string {
   return `#compdef daemon
 
 _daemon() {
-  local line
+  local line state
   local -a subcmds
   subcmds=(
     'create:Create and start a new daemon'
@@ -23,8 +23,8 @@ _daemon() {
     'completion:Generate shell completion scripts'
   )
 
-  _arguments -C 
-    "1: :->cmds" 
+  _arguments -C \\
+    "1: :->cmds" \\
     "*::arg:->args"
 
   case $state in
@@ -39,10 +39,10 @@ _daemon() {
           _values 'daemons' $daemons
           ;;
         create)
-          _arguments 
-            '--rotation[Log rotation interval]:interval:(daily weekly hourly)' 
-            '--keep[Number of log files to keep]' 
-            '--compress[Compress rotated logs]' 
+          _arguments \\
+            '--rotation[Log rotation interval]:interval:(daily weekly hourly)' \\
+            '--keep[Number of log files to keep]' \\
+            '--compress[Compress rotated logs]' \\
             '--no-keep-alive[Disable KeepAlive]'
           ;;
       esac
@@ -50,6 +50,6 @@ _daemon() {
   esac
 }
 
-_daemon "$@"
+compdef _daemon daemon
 `
 }
